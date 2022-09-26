c4bf8acd557b721463a536ccd7ce1824584fa563
masudrana03 <920mash@gmail.com>
2022-09-26 22:49:15 +0600

    TestController added.

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    // Hear is all Test Functions
+
+
+    public function test()
+    {
+        // nothing to test...
+    }
+}
